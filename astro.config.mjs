74d9178667f09fa7a617439ef70b9054569b1661
roberto <roberdex26@gmail.com>
2025-07-10 08:11:45 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: "https://romagaco.github.io",
-  base: "/helpless-heliosphere/", // Base path 
+  base: "/astro-2", // Base path 
   i18n: {
     defaultLocale: 'es', // Default language
     locales: ['es', 'en'], // Supported languages
